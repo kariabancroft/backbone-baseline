AdaGold/ada-trader

Empty application scaffold
This is slightly more than the bare scaffold, but mostly it just
contains an empty Application model and an associated view that says
"Hello World".

There is also a basic spec to give the students an indication of
where/how they should setup their specs.

src/app.js
@@ -1,0 +1,9 @@
+import Application from 'app/models/application';
+import ApplicationView from 'app/views/application_view';
+
+var application = new Application();
+
+var appView = new ApplicationView({
+  el: '#mount',
+  model: application
+});

kariabancroft/backbone-baseline

src/app.js
@@ -1,5 +1,13 @@
 import $ from 'jquery';
+import Application from 'app/models/application';
+import ApplicationView from 'app/views/application_view';
 
 $(document).ready(function() {
-  // Write your application code here
+  var application = new Application();
+
+  var appView = new ApplicationView({
+    el: '#application',
+    model: application
+  });
+  appView.render();
 });